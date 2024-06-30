8cb4f817e54a6e1c49cf5011a25a9b4a116ebc55
mujeebullahkalhoro <mujeebullahkalhoro77@gamil.com>
2024-07-01 00:46:24 +0500

    side bar is completed here

---
+++ index.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const bars = document.querySelector('.bars');
+    const sidebar = document.querySelector('.sidebar');
+    
+    bars.addEventListener('click', function(e) {
+      e.preventDefault();
+      sidebar.classList.toggle('show');
+    });
+    
+    document.addEventListener('click', function(e) {
+      if (!sidebar.contains(e.target) && !bars.contains(e.target)) {
+        sidebar.classList.remove('show');
+      }
+    });
+  });
+  
